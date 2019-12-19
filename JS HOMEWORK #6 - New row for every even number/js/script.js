664099f11df9b20c96a53284e72a7dda1ffeaa9c
Cakizy <58572124+Cakizy@users.noreply.github.com>
2019-12-20 00:39:18 +0100

    JS HOMEWORK #6 - New row for every even number
    JS HOMEWORK #6 - New row for every even number

---
+++ JS HOMEWORK #6 - New row for every even number/js/script.js
@@ -0,0 +1,5 @@
+for (let num = 1; num <= 20; num++) {
+    if (num % 2 === 1) {
+        console.log(num + ` ` + `${num+1}` /*+ `\n` */ ); //Ne gledam pricina zosto  da stoi i toa console.log ja vrsi istata funkcija (\n)
+    }
+}
