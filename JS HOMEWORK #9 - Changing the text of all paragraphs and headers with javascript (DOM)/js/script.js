339b43052477975075f2605d280d323f4fe08bbe
Cakizy <58572124+Cakizy@users.noreply.github.com>
2019-12-20 02:04:58 +0100

    Update script.js

--- JS HOMEWORK #9 - Changing the text of all paragraphs and headers with javascript (DOM)/js/script.js
+++ JS HOMEWORK #9 - Changing the text of all paragraphs and headers with javascript (DOM)/js/script.js
@@ -1,10 +1,5 @@
-let allParagraphs = document.querySelectorAll(`P`);
+let allParagraphs = document.querySelectorAll(`P, h1, h3`);
 for (let i = 0; i < allParagraphs.length; i++) {
     allParagraphs[i].innerText = ``;
     allParagraphs[i].innerText = `Smart thinking ...`;
-}
-let allHeaders = document.querySelectorAll(`h1, h3`);
-for (let i = 0; i < allHeaders.length; i++) {
-    allHeaders[i].innerText = ``;
-    allHeaders[i].innerText = `Smart thinking ...`;
 }
